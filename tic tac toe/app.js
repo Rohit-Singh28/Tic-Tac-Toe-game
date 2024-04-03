834bf501782f03c10011430ae6c5b2e585066668
Rohit-Singh28 <162868531+Rohit-Singh28@users.noreply.github.com>
2024-04-03 17:05:24 +0530

    Update app.js

--- tic tac toe/app.js
+++ tic tac toe/app.js
@@ -5,7 +5,7 @@ let outerbox =document.querySelector(".outerBox");
 
 let turn = "x";
 let game = true;
-// let k = 0 ;
+
 
 const chanceTurn = () => {
 
@@ -24,7 +24,7 @@ function winner(){
         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6],[0,4,8]
     ]
     wins.forEach(e => {
-        // console.log(text[2]);
+        
         if (
             (text[e[0]].innerText === text[e[1]].innerText) &&
             (text[e[1]].innerText === text[e[2]].innerText) &&
@@ -41,7 +41,7 @@ function winner(){
                 outerbox.classList.add("win");
 
             }
-            // k = 1 ;
+           
         }
     })
 }
@@ -50,16 +50,12 @@ function winner(){
 for (box of boxes) {
         let text = box.querySelector(".textBox");
         box.addEventListener("click", () => {
-            // console.log(text);
-            // console.log("clicked");
+           
             if (text.innerText === "") {
                 text.innerText = turn;
                 chanceTurn();
                 para.innerText = `${turn} chance`;
-                // if(k === 1){
-                //     text.innerText = '';
-                //     console.log("A");
-                // }
+
                 winner();
             }
         })
